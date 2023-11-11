110a8ae9c928149476173c7635b3f8b9ab15c0ac
Pubudu Bandara <pubudubandara09@gmail.com>
2023-11-11 12:34:44 -0500

    Add project files.

---
+++ Assign2_866199/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Assign2_866199.Models
+{
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set;}
+        public string? Department { get; set;}
+        public Char Sex { get; set;}
+
+    }
+}
---
+++ Assign2_866199/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assign2_866199.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
